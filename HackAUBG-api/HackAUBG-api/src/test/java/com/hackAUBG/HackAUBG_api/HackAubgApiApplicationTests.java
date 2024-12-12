89aa3308d79eec67e0dd75b4c7b780d61e28a541
Belis Aliosmanova <benidebelis@gmail.com>
2024-12-12 21:00:33 +0200

    Initial commit

---
+++ HackAUBG-api/HackAUBG-api/src/test/java/com/hackAUBG/HackAUBG_api/HackAubgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackAUBG.HackAUBG_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackAubgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
